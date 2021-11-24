1b9fc76d0a4fcef14a8b2817ff02232ea9e5fed0
Yoon <respect5716@gmail.com>
2021-11-24 08:20:20 +0000

    UDATE

--- src/model/distil_bert.py
+++ src/model/distil_bert.py
@@ -4,3 +4,6 @@ import pytorch_lightning as pl
 class Model(pl.LightningModule):
     def __init__(self, *args, **kwargs):
         super().__init__()
+        self.save_hyperparameters()
+
+        print(self.hparams.student.hidden_size)
